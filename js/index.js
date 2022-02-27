c40e813e88105731d27aa2b2b52ff4bf089d7bed
DizZer0 <96202804+DizZer0@users.noreply.github.com>
2022-02-27 16:12:39 +0300

    hotfix: изменить попап, форму и profile
    поменять структуру popap, привязать сохранение формы к форме, добавить переполнение для элементов profile__name, profile__status, добавить hover кнопкам в попапе

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-let editForm = document.querySelector('.edit-form');
+let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
-editButton.addEventListener('click', openEditForm);
-let exitButton = editForm.querySelector('.edit-profile__exit-button');
-exitButton.addEventListener('click', closeEditForm);
+let exitButton = popup.querySelector('.popup__exit-button');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__status');
+let editName = popup.querySelector('.edit-profile__text_input_name');
+let editJob = popup.querySelector('.edit-profile__text_input_job');
+let saveButton = popup.querySelector('.edit-profile__save-button');
+let editForm = popup.querySelector('.edit-profile');
 function closeEditForm() {
-  editForm.classList.add('edit-form_disabled');
+  popup.classList.add('popup_disabled');
 }
 function openEditForm() {
-  editForm.classList.remove('edit-form_disabled');
+  popup.classList.remove('popup_disabled');
   editName.value = profileName.textContent;
   editJob.value = profileJob.textContent;
 }
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__status');
-let editName = editForm.querySelector('.edit-profile__name-text');
-
-let editJob = editForm.querySelector('.edit-profile__job-text');
-let saveButton = editForm.querySelector('.edit-profile__save-button');
-saveButton.addEventListener('click', saveEditForm);
 function saveEditForm() {
-  editForm.classList.add('edit-form_disabled');
+  popup.classList.add('popup_disabled');
   profileName.textContent = editName.value;
   profileJob.textContent = editJob.value;
 }
+editForm.addEventListener('submit', saveEditForm);
+exitButton.addEventListener('click', closeEditForm);
+editButton.addEventListener('click', openEditForm);
